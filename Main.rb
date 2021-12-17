b4b61de0724e72ac99a8b6c73feedd1a54d7f655
Kyle Corlett <corlettkyle5@hotmail.com>
2021-12-17 12:26:38 +1000

    Added main menu and title sequence.

--- Main.rb
+++ Main.rb
@@ -6,5 +6,40 @@ prompt = TTY::Prompt.new(active_color: :bright_magenta)
 pastel = Pastel.new
 font = TTY::Font.new(:doom)
 
-puts pastel.magenta(font.write("Connect Four"))
+while true
 
+puts pastel.yellow("
+    ████████████████████████████████████████████████████████████████████████
+    █─▄▄▄─█─▄▄─█▄─▀█▄─▄█▄─▀█▄─▄█▄─▄▄─█─▄▄▄─█─▄─▄─███▄─▄▄─█─▄▄─█▄─██─▄█▄─▄▄▀█
+    █─███▀█─██─██─█▄▀─███─█▄▀─███─▄█▀█─███▀███─██████─▄███─██─██─██─███─▄─▄█
+    ▀▄▄▄▄▄▀▄▄▄▄▀▄▄▄▀▀▄▄▀▄▄▄▀▀▄▄▀▄▄▄▄▄▀▄▄▄▄▄▀▀▄▄▄▀▀▀▀▄▄▄▀▀▀▄▄▄▄▀▀▄▄▄▄▀▀▄▄▀▄▄▀")
+
+menu = prompt.select("Welcome to connect four", ["Start new game", "Rules", "Difficulty", "Exit"])
+
+case
+
+when menu == "Start new game"
+    print "Whats your name?"
+    name = gets.chomp.capitalize
+
+    if name != ""
+        system "clear"
+        puts "Welcome #{name}, prepare to battle to the top!"
+        sleep (1)
+        main_game
+    elsif name == ""
+        name = "Frank"
+        puts "Welcome #{name}, prepare to battle to the top!"
+        sleep (1)
+        main_game
+    end 
+
+when menu == "Rules"
+    rules
+when menu == "Difficulty"
+    select_difficulty
+when menu == "Exit"
+    system "clear"
+    return
+end
+end
